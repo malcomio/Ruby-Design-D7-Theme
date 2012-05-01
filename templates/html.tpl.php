e1f44042a6efd83c90b55228575b8785daf366ba
Malcolm Young <malcolm@rubydesign.co.uk>
2012-05-01 22:07:27 +0100

    added geo meta tags

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -1,18 +1,22 @@
 <?php print $doctype; ?>
 <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf->version . $rdf->namespaces; ?>>
-<head<?php print $rdf->profile; ?>>
-  <?php print $head; ?>
-  <title><?php print $head_title; ?></title>  
-  <?php print $styles; ?>
-  <?php print $scripts; ?>
-  <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-</head>
-<body<?php print $attributes;?>>
-  <div id="skip-link">
-    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
-  </div>
-  <?php print $page_top; ?>
-  <?php print $page; ?>
-  <?php print $page_bottom; ?>
-</body>
+  <head<?php print $rdf->profile; ?>>
+    <?php print $head; ?>
+    <title><?php print $head_title; ?></title>  
+    <?php print $styles; ?>
+    <?php print $scripts; ?>
+    <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <meta name="geo.region" content="GB-SWK" />
+    <meta name="geo.placename" content="London" />
+    <meta name="geo.position" content="51.459699;-0.066597" />
+    <meta name="ICBM" content="51.459699, -0.066597" />
+  </head>
+  <body<?php print $attributes; ?>>
+    <div id="skip-link">
+      <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+    </div>
+    <?php print $page_top; ?>
+    <?php print $page; ?>
+    <?php print $page_bottom; ?>
+  </body>
 </html>
