2f7a8ebb7e503d21cfb47d59736cf04c814b0b2a
Malcolm Young <malcolm@rubydesign.co.uk>
2012-07-15 10:37:59 +0100

    added chrome frame header and ie7 message

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -10,11 +10,19 @@
     <meta name="geo.placename" content="London" />
     <meta name="geo.position" content="51.459699;-0.066597" />
     <meta name="ICBM" content="51.459699, -0.066597" />
+    <meta http-equiv="X-UA-Compatible" content="chrome=1">
   </head>
   <body<?php print $attributes; ?>>
     <div id="skip-link">
       <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
     </div>
+    <!--[if lt IE 8]>
+    <div class="ie7-message">
+      <h2>You are using an out-of-date-browser</h2>
+      <p>You will notice that our site might look a little odd in places. This is because you're using an unsupported browser.</p>
+      <p>For more information on what this means, please visit <a href="http://browsingbetter.com">http://browsingbetter.com</a></p>
+    </div>
+    <![endif]-->
     <?php print $page_top; ?>
     <?php print $page; ?>
     <?php print $page_bottom; ?>
